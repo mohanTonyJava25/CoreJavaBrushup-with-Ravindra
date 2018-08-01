643309a7034fa63926e30ddce09fed60c63c26bf
mohanTonyJava25 <mohan.paralla@hotmail.com>
2018-07-31 22:05:24 -0400

    added chicken class

---
+++ CoreJava/src/oops/chicken.java
@@ -0,0 +1,13 @@
+package oops;
+
+public class chicken extends bird {
+
+	public static void main (String args[])
+	{
+		System.out.println("chicken class");
+		
+		chicken chik = new chicken();
+		chik.eat();
+	}
+	
+}
