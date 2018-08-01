c0cf8710cab05ae06cee51144cf773c8306c31bd
mohanTonyJava25 <mohan.paralla@hotmail.com>
2018-07-31 23:38:18 -0400

    collection arraylist checkin

---
+++ CoreJava/src/collections/arraylistExample.java
@@ -0,0 +1,37 @@
+package collections;
+
+import java.util.*;
+
+public class arraylistExample {
+ public static void main(String args[])
+ {
+	  ArrayList<Integer> list = new ArrayList<Integer>();
+    	  
+//	array list allows duplicates and it is in order . 
+//	check the example. 
+//	array list is growable in size. you can check it with the list.size  
+//	this is sample example of arraylist  
+	  
+	list.add(24);
+	list.add(25);
+	list.add(24);
+	list.add(12);
+	list.add(25);
+	list.add(24);
+	list.add(50);
+	list.add(49);
+	list.add(35);
+	list.add(35);
+	list.add(35);
+	  
+	    for( int i=0; i<list.size(); i++)
+	    {
+	System.out.println(list.get(i));
+	
+	    }
+	System.out.println(list.size());
+	
+	  
+ }   
+ 
+}
